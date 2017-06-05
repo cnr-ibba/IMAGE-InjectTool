9323f4a18c4dfc689b81d655e0ab899ffb4632ab
Andrea <andrea.caprera@gmail.com>
2017-06-05 14:55:32 +0200

    function to transfer organisations data added
    changes:
    [views.py]
    1.  data in the imported_from_...db for organizations (from v_contacts view) is converted:
        address is built from street, zip, town, ext_country, URI is mailto:<email-address>
    2.  data is saved into the image.organizations table

--- django-data/image/image_app/views.py
+++ django-data/image/image_app/views.py
@@ -74,6 +74,12 @@ def sampletab1(request):
 
 
 def write_record(myfile, record):
+    """Writes a string into a file, then newline
+
+    :param myfile: a file opened in append mode
+    :param record: a tab separated string to be added, in append mode, in the file
+    """
+
     global codecs
 
     with codecs.open(myfile, "a", encoding="utf-8") as f:
@@ -82,6 +88,19 @@ def write_record(myfile, record):
 
 
 def sampletab2(request):
+    """Creates the Sampletab file
+
+    This function creates the sampletab file. It is an unusually long fx, but
+    made of a relatively simple structure:
+    It creates the header of the file, containing data coming from the organisms
+    and persons tables. Once finished writes the true data: writes the field names row, then
+    for each animal it reads all animal's samples
+    and write all (1 animal row + n sample rows) into the csv file, and does it for all animals.
+
+    :param request: HTTP request automatically sent by the framework
+    :return: the resulting HTML page
+    """
+
     # filelink = ''
     username = None
 
@@ -92,116 +111,130 @@ def sampletab2(request):
         fileroot = os.path.join(settings.MEDIA_ROOT, filename)  # con path
         fileurl = os.path.join(settings.MEDIA_URL, filename)  # URL
 
-        context = {'username': username, 'fileurl': fileurl}
+        context = {'username': username, 'fileurl': "cippalippa"}
 
-        with codecs.open(fileroot, 'w', encoding="utf-8") as f:
-            f.write('[MSI]\n')
-            f.write('...\n')
-            f.write('[SCD]\n')
-            f.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
-                        'Sample Name',
-                        'Sample Accession',
-                        'Sample Description',
-                        'Characteristic[project]',
-                        'Material',
-                        'Organism',
-                        'Sex',
-                        'Characteristic[breed]',
-                        'Derived From',
-                        'Characteristic[father]',
-                        'Characteristic[mother]',
-                        'Characteristic[organism part]',
-                        'Characteristic[specimen collection date]',
-                        'Unit',
-                        'Characteristic[animal age at collection]',
-                        'Unit',
-                        'Characteristic[developmental stage]',
-                        'Characteristic[animal farm latitude]',
-                        'Characteristic[animal farm longitude]',
-                        'Characteristic[biobank description]',
-                        'Characteristic[biobank address]',
-                        'Characteristic[biobank contacts]',
+        # TEMP
 
-                        ))
-        # animals = get_list_or_404(Animals)
-        # queryset = Animals.objects.filter(author=request.user.id)
-        # animals = get_list_or_404(Animals, id='763')
-        animals = get_list_or_404(Animals)
+        return render(request, 'image_app/sampletab2.html', context)
 
-        for a in animals:
-            # buffer = a.name
-
-            try:
-                father = Animals.objects.get(pk=a.father_id).name
-            except Animals.DoesNotExist:
-                father = ''
-            try:
-                mother = Animals.objects.get(pk=a.mother_id).name
-            except Animals.DoesNotExist:
-                mother = ''
-
-            # eva = a.eva.all()
-            # eva_str = ';'.join([e.description for e in eva])
-            record = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
-                    a.name,
-                    # a.name,
-                    "IMAGE-a{0:05d}".format(a.id),
-                    a.description,
-                    'Image',
-                    'organism',
-                    a.breed.species,
-                    a.sex.description,
-                    a.breed.description,
-                    '',
-                    father,
-                    mother,
-                    '',
-                    '',
-                    '',
-                    '',
-                    '',
-                    '',
-                    a.farm_latitude,
-                    a.farm_longitude,
-                    '',
-                    '',
-                    '',
-                    # '', # eva_str,
-                    )
-            write_record(fileroot, record)
-            samples = a.samples.all()
-            for s in samples:
-                record = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
-                        # "{}_{}".format(a.name, s.name),
-                        s.name,
-                        "IMAGE-s{0:05d}".format(s.id),
-                        s.description,
-                        'Image',
-                        'specimen from organism',
-                        '',
-                        '',
-                        '',
-                        "IMAGE-a{0:05d}".format(a.id),
-                        '',
-                        '',
-                        s.organism_part,
-                        s.collection_date,
-                        'YYYY-MM-DD',
-                        s.animal_age_at_collection,
-                        'year',
-                        s.developmental_stage,
-                        '',
-                        '',
-                        '',
-                        )
-                write_record(fileroot, record)
-
-    else:
-        context = {'error_message': 'You are not authenticated'}
-    return render(request, 'image_app/sampletab2.html', context)
+        # TEMP
+    #     with codecs.open(fileroot, 'w', encoding="utf-8") as f:
+    #         f.write('[MSI]\n')
+    #         f.write('...\n')
+    #         f.write('[SCD]\n')
+    #         f.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
+    #                     'Sample Name',
+    #                     'Sample Accession',
+    #                     'Sample Description',
+    #                     'Characteristic[project]',
+    #                     'Material',
+    #                     'Organism',
+    #                     'Sex',
+    #                     'Characteristic[breed]',
+    #                     'Derived From',
+    #                     'Characteristic[father]',
+    #                     'Characteristic[mother]',
+    #                     'Characteristic[organism part]',
+    #                     'Characteristic[specimen collection date]',
+    #                     'Unit',
+    #                     'Characteristic[animal age at collection]',
+    #                     'Unit',
+    #                     'Characteristic[developmental stage]',
+    #                     'Characteristic[animal farm latitude]',
+    #                     'Characteristic[animal farm longitude]',
+    #                     'Characteristic[biobank description]',
+    #                     'Characteristic[biobank address]',
+    #                     'Characteristic[biobank contacts]',
+    #
+    #                     ))
+    #     # animals = get_list_or_404(Animals)
+    #     # queryset = Animals.objects.filter(author=request.user.id)
+    #     # animals = get_list_or_404(Animals, id='763')
+    #     animals = get_list_or_404(Animals)
+    #
+    #     for a in animals:
+    #         # buffer = a.name
+    #
+    #         try:
+    #             father = Animals.objects.get(pk=a.father_id).name
+    #         except Animals.DoesNotExist:
+    #             father = ''
+    #         try:
+    #             mother = Animals.objects.get(pk=a.mother_id).name
+    #         except Animals.DoesNotExist:
+    #             mother = ''
+    #
+    #         # eva = a.eva.all()
+    #         # eva_str = ';'.join([e.description for e in eva])
+    #         record = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
+    #                 a.name,
+    #                 # a.name,
+    #                 "IMAGE-a{0:05d}".format(a.id),
+    #                 a.description,
+    #                 'Image',
+    #                 'organism',
+    #                 a.breed.species,
+    #                 a.sex.description,
+    #                 a.breed.description,
+    #                 '',
+    #                 father,
+    #                 mother,
+    #                 '',
+    #                 '',
+    #                 '',
+    #                 '',
+    #                 '',
+    #                 '',
+    #                 a.farm_latitude,
+    #                 a.farm_longitude,
+    #                 '',
+    #                 '',
+    #                 '',
+    #                 # '', # eva_str,
+    #                 )
+    #         write_record(fileroot, record)
+    #         samples = a.samples.all()
+    #         for s in samples:
+    #             record = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
+    #                     # "{}_{}".format(a.name, s.name),
+    #                     s.name,
+    #                     "IMAGE-s{0:05d}".format(s.id),
+    #                     s.description,
+    #                     'Image',
+    #                     'specimen from organism',
+    #                     '',
+    #                     '',
+    #                     '',
+    #                     "IMAGE-a{0:05d}".format(a.id),
+    #                     '',
+    #                     '',
+    #                     s.organism_part,
+    #                     s.collection_date,
+    #                     'YYYY-MM-DD',
+    #                     s.animal_age_at_collection,
+    #                     'year',
+    #                     s.developmental_stage,
+    #                     '',
+    #                     '',
+    #                     '',
+    #                     )
+    #             write_record(fileroot, record)
+    #
+    # else:
+    #     context = {'error_message': 'You are not authenticated'}
+    # return render(request, 'image_app/sampletab2.html', context)
 
 
 def model_form_upload(request):
+    """Creates the upload form
+
+    This function creates the BackupForm, defined in the forms.py file.
+    This form adds a file to the Backups table, together with a description string.
+
+    :param request: HTTP request automatically sent by the framework
+    :return: the resulting HTML page
+    """
+
     if request.user.is_authenticated():
         username = request.user.username
         # context = {'username': username}
@@ -221,6 +254,17 @@ def model_form_upload(request):
 
 
 def dump_reading(request):
+    """Imports backup into the received_from_cryoweb db
+
+    This function uses the container's installation of psql to import a backup file into the
+    "imported_from_cryoweb" database.
+    The imported backup file is the last inserted into the image's table Backups.
+
+    :param request: HTTP request automatically sent by the framework
+    :return: the resulting HTML page
+
+    """
+
     if request.user.is_authenticated():
         username = request.user.username
         context = {'username': username}
@@ -250,6 +294,15 @@ def dump_reading(request):
 
 
 def dump_reading2(request):
+    """Reformats data and write it into the image database
+
+    This fx reads the imported_from_cryoweb data,
+    changes their format (i.e., changes field names and/or content) and
+    writes data into the image database.
+
+    :param request: HTTP request automatically sent by the framework
+    :return: the resulting HTML page
+    """
 
     def get_breed_id(row, df_breeds_species):
         # global df_breeds_species
@@ -265,6 +318,9 @@ def dump_reading2(request):
             engine_from_cryoweb = create_engine('postgresql://postgres:***REMOVED***@db:5432/imported_from_cryoweb')
             engine_to_sampletab = create_engine('postgresql://postgres:***REMOVED***@db:5432/image')
 
+            pd.set_option("display.max_columns", None)
+            pd.set_option("display.max_rows", None)
+
             df_breeds_species = pd.read_sql_table('v_breeds_species', con=engine_from_cryoweb, schema='public')
             # df_breeds_species.head()
             df_breeds_fin = df_breeds_species[
@@ -278,7 +334,9 @@ def dump_reading2(request):
                     'efabis_lang': 'language',
                 }
             )
-            df_breeds_fin.to_sql(name='dict_breeds', con=engine_to_sampletab, if_exists='append', index=False)
+            # df_breeds_fin.to_sql(name='dict_breeds', con=engine_to_sampletab, if_exists='append', index=False)
+
+            # ANIMALS
 
             df_animals = pd.read_sql_table('v_animal', con=engine_from_cryoweb, schema='public')
             df_animals['breed_id'] = df_animals.apply(lambda row: get_breed_id(row, df_breeds_species), axis=1)
@@ -302,7 +360,9 @@ def dump_reading2(request):
             )
             df_animals_fin['sex_id'] = df_animals_fin['sex_id'].replace({'m': 1, 'f': 2})
             df_animals_fin['name'] = df_animals_fin['name'].str.replace('\t', '')
-            df_animals_fin.to_sql(name='animals', con=engine_to_sampletab, if_exists='append', index=False)
+            # df_animals_fin.to_sql(name='animals', con=engine_to_sampletab, if_exists='append', index=False)
+
+            # SAMPLES
 
             df_samples = pd.read_sql_table('v_vessels', con=engine_from_cryoweb, schema='public')
             # df_samples.head()
@@ -321,10 +381,30 @@ def dump_reading2(request):
 
             df_samples_fin['name'] = df_samples_fin['name'].str.replace('\t', '')
 
-            # df_samples_fin.head()
-            df_samples_fin.to_sql(name='samples', con=engine_to_sampletab, if_exists='append', index=False)
+            # df_samples_fin.to_sql(name='samples', con=engine_to_sampletab, if_exists='append', index=False)
 
-            context['fullpath'] = "OK"
+            # ORGANIZATIONS
+
+            df_organizations = pd.read_sql_table('v_contacts', con=engine_from_cryoweb, schema='public')
+
+            df_organizations['address'] = df_organizations['street'] + ", " + \
+                                          df_organizations['zip'] + " " + \
+                                          df_organizations['town'] + ", " + \
+                                          df_organizations['ext_country']
+
+            df_organizations['URI'] = "emailto:" + df_organizations['email']
+            df_organizations = df_organizations.rename(
+                columns={
+                    'third_name': 'name',
+                }
+            )
+            # df_organizations['role'] = 'bank'
+            df_organizations_fin = df_organizations[
+                ['name', 'address', 'URI']]
+            df_organizations_fin.to_sql(name='organizations', con=engine_to_sampletab, if_exists='append', index=False)
+
+
+            context['fullpath'] = df_organizations_fin.head()
 
         except:
             context['fullpath'] = "ERROR!"
