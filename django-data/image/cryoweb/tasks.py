e8840778d95321dfdab463466512259da8561f93
Paolo Cozzi <bunop@libero.it>
2018-09-13 14:34:11 +0200

    :hammer: refactoring cryoweb task locks

--- django-data/image/cryoweb/tasks.py
+++ django-data/image/cryoweb/tasks.py
@@ -4,24 +4,58 @@
 Created on Thu Sep 13 11:33:09 2018
 
 @author: Paolo Cozzi <cozzi@ibba.cnr.it>
+
+Inspired from
+
+http://docs.celeryproject.org/en/latest/tutorials/task-cookbook.html
+
 """
 
 from time import sleep
-from celery.utils.log import get_task_logger
 
-from image.celery import app as celery_app, only_one, MyTask
+from celery import task
+from celery.five import monotonic
+from celery.utils.log import get_task_logger
+from contextlib import contextmanager
+from django.core.cache import cache
+
 
 logger = get_task_logger(__name__)
 
+LOCK_EXPIRE = 60 * 10  # Lock expires in 10 minutes
 
-# Ok define a task for uploading cryoweb data in a exclusive way. Need
-# submission_id as parameter
-@celery_app.task(bind=True, base=MyTask)
-@only_one(key="SingleTask", timeout=60 * 10, blocking=True)
+
+@contextmanager
+def memcache_lock(lock_id, oid):
+    timeout_at = monotonic() + LOCK_EXPIRE - 3
+    # cache.add fails if the key already exists
+    status = cache.add(lock_id, oid, LOCK_EXPIRE)
+
+    try:
+        yield status
+
+    finally:
+        # memcache delete is very slow, but we have to use it to take
+        # advantage of using add() for atomic locking
+        if monotonic() < timeout_at and status:
+            # don't release the lock if we exceeded the timeout
+            # to lessen the chance of releasing an expired lock
+            # owned by someone else
+            # also don't release the lock if we didn't acquire it
+            cache.delete(lock_id)
+
+
+@task(bind=True)
 def import_from_cryoweb(self, submission_id):
+    # The cache key consists of the task name and the MD5 digest
+    # of the feed URL.
+    lock_id = 'ImportFromCryoWeb'
     logger.info("Start import from cryoweb for submission: %s" % submission_id)
 
-    # do some stuff
-    sleep(60)
+    with memcache_lock(lock_id, self.app.oid) as acquired:
+        if acquired:
+            # do some stuff
+            sleep(60)
 
-    logger.info("Cryoweb import completed for submission: %s" % submission_id)
+    logger.info(
+        "Cryoweb import completed for submission: %s" % submission_id)
