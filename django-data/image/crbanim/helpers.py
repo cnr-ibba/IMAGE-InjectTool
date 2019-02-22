51718c2881ecb3872769d2a9344d5325912a0f33
Paolo Cozzi <bunop@libero.it>
2019-02-22 17:49:13 +0100

    :white_check_mark: testing crbanim.helpers methods

--- django-data/image/crbanim/helpers.py
+++ django-data/image/crbanim/helpers.py
@@ -6,3 +6,149 @@ Created on Thu Feb 21 15:37:16 2019
 @author: Paolo Cozzi <cozzi@ibba.cnr.it>
 """
 
+import csv
+import logging
+
+from collections import defaultdict
+
+from common.constants import LOADED, ERROR
+from image_app.models import DictSpecie, DictSex
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+# A class to deal with cryoweb import errors
+class CRBAnimImportError(Exception):
+    pass
+
+
+class CRBAnimReader():
+    def __init__(self):
+        self.data = None
+        self.header = None
+        self.dialect = None
+        self.items = None
+
+    def read_file(self, filename):
+        """Read crb anim files and set tit to class attribute"""
+
+        with open(filename, newline='') as csvfile:
+            self.dialect = csv.Sniffer().sniff(csvfile.read(2048))
+            csvfile.seek(0)
+            reader = csv.reader(csvfile, self.dialect)
+            self.header = next(reader)
+            self.data = list(reader)
+
+        self.items = self.eval_columns()
+
+    def eval_columns(self):
+        """define a set from column data"""
+
+        # target_columns = ['sex', 'species_latin_name', 'breed_name']
+        target_columns = self.header
+
+        items = defaultdict(list)
+
+        for line in self.data:
+            for column in target_columns:
+                idx = self.header.index(column)
+                items[column].append(line[idx])
+
+        # now get a set of object
+        for column in target_columns:
+            items[column] = set(items[column])
+
+        return items
+
+    def print_line(self, num):
+        """print a record with its column names"""
+
+        for i, column in enumerate(self.header):
+            logger.debug("%s: %s" % (column, self.data[num][i]))
+
+    def __check_items(self, item_set, model, column):
+        """General check of CRBanim items into database"""
+
+        not_found = []
+
+        for item in item_set:
+            # check for species in database
+            if not model.objects.filter(label=item).exists():
+                not_found.append(item)
+
+        if len(not_found) == 0:
+            return True
+
+        else:
+            logger.error(
+                "Those %s are not present in UID database:" % (column))
+            logger.error(not_found)
+
+            return False
+
+    # a function to detect if crbanim species are in UID database or not
+    # HINT: crbanim species are already in the form required from DictSpecies
+    # tables, no need to search for language
+    def check_species(self):
+        """Check if all species are defined in UID DictSpecies"""
+
+        column = 'species_latin_name'
+
+        return self.__check_items(self.items[column], DictSpecie, column)
+
+    # check that dict sex table contains data
+    def check_sex(self):
+        """check that dict sex table contains data"""
+
+        # item.sex are in uppercase
+        column = 'sex'
+        item_set = [item.lower() for item in self.items[column]]
+
+        return self.__check_items(item_set, DictSex, column)
+
+
+def upload_crbanim(submission):
+    # debug
+    logger.info("Importing from CRB-Anim file")
+
+    # this is the full path in docker container
+    fullpath = submission.get_uploaded_file_path()
+
+    # read submission data
+    reader = CRBAnimReader()
+    reader.read_file(fullpath)
+
+    # start data loading
+    try:
+        # check for species and sex like cryoweb does
+        if not reader.check_sex():
+            raise CRBAnimImportError("You have to upload DictSex data")
+
+        if not reader.check_species():
+            raise CRBAnimImportError(
+                "Some species are not loaded in UID database")
+
+    except Exception as exc:
+        # save a message in database
+        submission.status = ERROR
+        submission.message = "Error in importing data: %s" % (str(exc))
+        submission.save()
+
+        # debug
+        logger.error("error in importing from crbanim: %s" % (exc))
+        logger.exception(exc)
+
+        return False
+
+    else:
+        message = "CRBAnim import completed for submission: %s" % (
+            submission.id)
+
+        submission.message = message
+        submission.status = LOADED
+        submission.save()
+
+    logger.info("Import from CRBAnim is complete")
+
+    return True
--- django-data/image/crbanim/tests/test_helpers.py
+++ django-data/image/crbanim/tests/test_helpers.py
@@ -6,26 +6,32 @@ Created on Fri Feb 22 15:49:18 2019
 @author: Paolo Cozzi <cozzi@ibba.cnr.it>
 """
 
+import logging
+from unittest.mock import patch
+
 from django.test import TestCase
 
-from common.constants import ERROR
+from common.constants import ERROR, LOADED
 from common.tests import DataSourceMixinTestCase
-from image_app.models import Submission, db_has_data
+from image_app.models import Submission, db_has_data, DictSpecie, DictSex
 
-from ..helpers import upload_crbanim, CRBAnimImportError
+from ..helpers import (
+    logger, CRBAnimReader, upload_crbanim, CRBAnimImportError)
 
 
-class BaseTestCase():
+class BaseTestCase(DataSourceMixinTestCase):
+    # define attribute in DataSourceMixinTestCase
+    model = Submission
+
     # import this file and populate database once
     fixtures = [
+        'crbanim/dictspecie',
+        'crbanim/submission',
         'image_app/dictcountry',
         'image_app/dictrole',
         'image_app/dictsex',
         'image_app/organization',
-        'crbanim/submission',
-        'image_app/user',
-        'language/dictspecie',
-        'language/speciesynonim'
+        'image_app/user'
     ]
 
     def setUp(self):
@@ -36,12 +42,122 @@ class BaseTestCase():
         self.submission = Submission.objects.get(pk=1)
 
 
-class UploadCRBAnim(DataSourceMixinTestCase, BaseTestCase, TestCase):
-    # define attribute in DataSourceMixinTestCase
-    model = Submission
+class CRBAnimReaderTestCase(BaseTestCase, TestCase):
+    def setUp(self):
+        # calling my base class setup
+        super().setUp()
+
+        # crate a CRBAnimReade object
+        self.reader = CRBAnimReader()
+
+        # get filenames for DataSourceMixinTestCase.dst_path
+        self.reader.read_file(self.dst_path)
+
+    def test_has_columns(self):
+        """Test if class has columns"""
+
+        reference = [
+            'BRC_identifier',
+            'BRC_name',
+            'associated_research_project_name',
+            'BRC_responsible_last_name',
+            'BRC_responsible_first_name',
+            'BRC_contact_email',
+            'organization_name',
+            'organisation_adress',
+            'organization_url',
+            'organization_country',
+            'BRC_address',
+            'BRC_url',
+            'BRC_country',
+            'sample_identifier',
+            'EBI_Biosample_identifier',
+            'animal_ID',
+            'sex',
+            'species_latin_name',
+            'NCBI_taxo_id',
+            'breed_name',
+            'country_of_origin',
+            'sampling_protocol_url',
+            'sampling_date',
+            'sample_type_name',
+            'sample_type_identifier',
+            'sample_type_ontology',
+            'body_part_name',
+            'body_part_identifier',
+            'body_part_ontology',
+            'animal_birth_date',
+            'sample_storage_temperature',
+            'sample_container_type',
+            'sample_fixer_nature']
+
+        self.assertEqual(reference, self.reader.header)
+
+    def test_debug_row(self):
+        """Assert a function is callable"""
+
+        self.assertLogs(logger=logger, level=logging.DEBUG)
+
+    def test_check_species(self):
+        """Test check species method"""
+
+        self.assertTrue(self.reader.check_species())
+
+        # changing species set
+        DictSpecie.objects.filter(label='Bos taurus').delete()
+
+        # the read species are not included in fixtures
+        self.assertFalse(self.reader.check_species())
+
+    def test_check_sex(self):
+        """Test check sex method"""
+
+        self.assertTrue(self.reader.check_sex())
+
+        # changing sex set
+        DictSex.objects.filter(label='female').delete()
+
+        # the read species are not included in fixtures
+        self.assertFalse(self.reader.check_sex())
+
+
+class UploadCRBAnimTestCase(BaseTestCase, TestCase):
 
     def test_upload_crbanim(self):
         """Testing uploading and importing data from crbanim to UID"""
 
-        self.assertTrue(upload_crbanim(self.submission.id))
+        self.assertTrue(upload_crbanim(self.submission))
+
+        # assert submission messages
+        self.assertEqual(
+            self.submission.status,
+            LOADED)
+
+        self.assertIn(
+            "CRBAnim import completed for submission",
+            self.submission.message)
+
+        # assert data into database
         self.assertTrue(db_has_data())
+
+    @patch("crbanim.helpers.CRBAnimReader.check_species",
+           side_effect=Exception("Test message"))
+    def test_upload_crbanim_errors(self, my_check):
+        """Testing importing with data into UID with errors"""
+
+        self.assertFalse(upload_crbanim(self.submission))
+        self.assertTrue(my_check.called)
+
+        # reload submission
+        self.submission = Submission.objects.get(pk=1)
+
+        self.assertEqual(
+            self.submission.status,
+            ERROR)
+
+        self.assertIn(
+            "Test message",
+            self.submission.message)
+
+        # assert data into database
+        self.assertFalse(db_has_data())
