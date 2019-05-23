3193c89d7ba28d019c2f4f26d15b16e7e06364d2
Alexey Sokolov <alexey@ebi.ac.uk>
2019-05-23 15:02:49 +0100

    Rewriting consumers in async manner

--- django-data/image/submissions_ws/consumers.py
+++ django-data/image/submissions_ws/consumers.py
@@ -1,46 +1,43 @@
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.submission_key = self.scope['url_route']['kwargs']['submission_key']
         self.room_group_name = 'submission_%s' % self.submission_key
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    def receive(self, text_data):
-        message = text_data
-
+    async def receive(self, text_data):
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'status_message',
-                'message': message
+                'message': text_data
             }
         )
 
     # Receive message from room group
-    def status_message(self, event):
+    async def status_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
