1e655aa94b6dc7a391dca2f835c6b6351fad3c23
Paolo Cozzi <bunop@libero.it>
2019-06-21 18:31:34 +0200

    :whale: fixing environment vaiables in docker-compose.yml
    See https://stackoverflow.com/a/56027496/4385116 for more info
    during issues in postgres connection

--- django-data/image/image_app/admin.py
+++ django-data/image/image_app/admin.py
@@ -122,8 +122,8 @@ class SampleAdmin(admin.ModelAdmin):
     fields = (
         ('name', 'alternative_id', 'description', 'owner'),
         ('animal', 'protocol', 'organism_part'),
-        ('collection_date', 'collection_place_latitude',
-         'collection_place_longitude', 'collection_place'),
+        ('collection_date', 'collection_place', 'collection_place_latitude',
+         'collection_place_longitude', 'collection_place_accuracy'),
         ('developmental_stage',
          'physiological_stage', 'animal_age_at_collection', 'availability'),
         ('storage_processing', 'preparation_interval')
