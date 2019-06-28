2314bdef925199295a3aa03d6ed7ba91dcf54974
Paolo Cozzi <bunop@libero.it>
2019-06-28 17:12:22 +0200

    :fire: remove dateencoder and decoder from common.helpers
    Could be restored from this commit if needed

--- django-data/image/common/helpers.py
+++ django-data/image/common/helpers.py
@@ -94,50 +94,6 @@ async def send_message_to_websocket(message, pk):
         await websocket.send(json.dumps(message))
 
 
-# Json encoder and decoder. Inspired from:
-# https://gist.githubusercontent.com/abhinav-upadhyay/5300137/raw/71cd3b1660d0c1919bcfb4d39692030431623eea/DateTimeDecoder.py
-# HINT: move to IMAGEEncoder?
-# HINT: read GO from database?
-class DateEncoder(json.JSONEncoder):
-    """ Instead of letting the default encoder convert datetime to string,
-        convert datetime objects into a dict, which can be decoded by the
-        DateDecoder
-    """
-
-    def default(self, obj):
-        # logger.debug(repr(obj))
-        if isinstance(obj, datetime.date):
-            return {
-                'text': obj.isoformat(),
-                'unit': "YYYY-MM-DD",
-            }
-        else:
-            return json.JSONEncoder.default(self, obj)
-
-
-class DateDecoder(json.JSONDecoder):
-    def __init__(self, *args, **kargs):
-        json.JSONDecoder.__init__(self, object_hook=self.dict_to_object,
-                                  *args, **kargs)
-
-    def dict_to_object(self, d):
-        """Call a different method when I found something like:
-        {"text": "1991-07-26", "unit": "YYYY-MM-DD"}"""
-
-        # return to caller if the keyword unit is not found
-        if 'unit' not in d:
-            return d
-
-        # try to read datatime
-        try:
-            dateobj = datetime.datetime.strptime(d['text'], '%Y-%m-%d').date()
-            return dateobj
-
-        # if not, raise esception
-        except Exception:
-            return d
-
-
 def format_attribute(value, terms=None, library_uri=OBO_URL, units=None):
     """Format a generic attribute into biosample dictionary"""
 
--- django-data/image/common/tests/test_helpers.py
+++ django-data/image/common/tests/test_helpers.py
@@ -6,17 +6,12 @@ Created on Wed Mar 27 14:35:36 2019
 @author: Paolo Cozzi <cozzi@ibba.cnr.it>
 """
 
-import datetime
-import json
-
 from django.test import TestCase
 from django.utils.dateparse import parse_date
 from django.core.validators import validate_email
 
 from .. import constants
-from ..helpers import (
-    DateDecoder, DateEncoder, format_attribute, get_admin_emails,
-    image_timedelta)
+from ..helpers import format_attribute, get_admin_emails, image_timedelta
 
 
 class TestImageTimedelta(TestCase):
@@ -81,65 +76,6 @@ class TestImageTimedelta(TestCase):
         self.assertEqual(units, constants.YEARS)
         self.assertEqual(len(cm.output), 1)
         self.assertIn("One date is NULL", cm.output[0])
-
-
-class JsonTestCase(TestCase):
-
-    def setUp(self):
-        """Initialize"""
-
-        # TODO: get a real object
-        self.obj = {
-                'name': 'Siems_0734_404357',
-                'project': 'IMAGE',
-                'collectionDate': datetime.date(1991, 7, 26)
-                }
-
-        self.json_str = ('{"name": "Siems_0734_404357", "project": "IMAGE", '
-                         '"collectionDate": {"text": "1991-07-26", "unit": '
-                         '"YYYY-MM-DD"}}')
-
-    def test_encode(self):
-        """Encode object as string"""
-
-        json_str = json.dumps(self.obj, cls=DateEncoder)
-        self.assertEqual(json_str, self.json_str)
-
-    def test_decode(self):
-        """Decode a (image) json string into a obj"""
-
-        obj = json.loads(self.json_str, cls=DateDecoder)
-        self.assertEqual(obj, self.obj)
-
-    def test_decode_without_unit(self):
-        """Decode a date without unit (returns a string)"""
-
-        obj = {
-                'name': 'Siems_0734_404357',
-                'project': 'IMAGE',
-                'collectionDate': "1991-07-26"
-                }
-
-        fake_str = ('{"name": "Siems_0734_404357", "project": "IMAGE", '
-                    '"collectionDate": "1991-07-26"}')
-
-        fake_obj = json.loads(fake_str, cls=DateDecoder)
-        self.assertEqual(fake_obj, obj)
-
-    def test_encode_a_str_date(self):
-        """Encode a string date"""
-
-        fake_obj = {
-                'name': 'Siems_0734_404357',
-                'project': 'IMAGE',
-                'collectionDate': "1991-07-26"
-                }
-
-        str_ = ('{"name": "Siems_0734_404357", "project": "IMAGE", '
-                '"collectionDate": "1991-07-26"}')
-
-        fake_str = json.dumps(fake_obj, cls=DateEncoder)
-        self.assertEqual(fake_str, str_)
 
 
 class TestAttributes(TestCase):
