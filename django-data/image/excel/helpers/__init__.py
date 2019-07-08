ad0ec3301642fa546fb8a1c2a0ca0b4ca38bc0d9
Paolo Cozzi <bunop@libero.it>
2019-07-08 11:25:34 +0200

    :white_check_mark: testing excel template methods

--- django-data/image/excel/helpers/__init__.py
+++ django-data/image/excel/helpers/__init__.py
@@ -6,8 +6,9 @@ Created on Fri Jul  5 16:30:14 2019
 @author: Paolo Cozzi <cozzi@ibba.cnr.it>
 """
 
-from .exceltemplate import ExcelTemplate
+from .exceltemplate import ExcelTemplate, TEMPLATE_COLUMNS
 from .exceptions import ExcelImportError
 from .fill_uid import upload_template
 
-__all__ = ["ExcelTemplate", "ExcelImportError", "upload_template"]
+__all__ = [
+    "ExcelTemplate", "TEMPLATE_COLUMNS", "ExcelImportError", "upload_template"]
--- django-data/image/excel/tests/test_helpers.py
+++ django-data/image/excel/tests/test_helpers.py
@@ -8,6 +8,7 @@ Created on Tue Jul  2 10:58:42 2019
 
 import types
 from collections import defaultdict
+from unittest.mock import patch, Mock
 
 from django.test import TestCase
 
@@ -16,7 +17,7 @@ from common.tests import WebSocketMixin
 from image_app.models import (
     Submission, Animal, Sample, DictCountry, DictSpecie, DictSex, db_has_data)
 
-from ..helpers import ExcelTemplate, upload_template
+from ..helpers import ExcelTemplate, upload_template, TEMPLATE_COLUMNS
 from .common import BaseExcelMixin
 
 
@@ -26,6 +27,8 @@ class ExcelTemplateTestCase(BaseExcelMixin, TestCase):
     def setUp(self):
         # calling my base class setup
         super().setUp()
+
+        self.maxDiff = None
 
         # crate a Excel Template object
         self.reader = ExcelTemplate()
@@ -40,6 +43,15 @@ class ExcelTemplateTestCase(BaseExcelMixin, TestCase):
         self.assertTrue(status)
         self.assertEqual(not_found, [])
 
+        # override sheet names
+        self.reader.sheet_names = []
+
+        # check method
+        status, not_found = self.reader.check_sheets()
+
+        self.assertFalse(status)
+        self.assertEqual(not_found, ['breed', 'animal', 'sample'])
+
     def test_check_columns(self):
         # test check sheets method
         status, not_found = self.reader.check_columns()
@@ -47,6 +59,60 @@ class ExcelTemplateTestCase(BaseExcelMixin, TestCase):
         self.assertTrue(status)
         self.assertIsInstance(not_found, defaultdict)
         self.assertEqual(len(not_found), 0)
+
+    @patch('xlrd.open_workbook')
+    def test_check_columns_issue(self, mock_open):
+        """Test a file with columns issues"""
+
+        # creating a mock excel book
+        mock_book = Mock()
+
+        # customizing the mock object
+        mock_book.sheet_names.return_value = ['breed', 'animal', 'sample']
+
+        # creating a mock sheet for breed
+        breed_sheet = Mock()
+        breed_sheet.nrows = 1
+
+        # now setting rows to a fake sheet
+        breed_sheet.row_values.return_value = []
+
+        # creating a mock sheet for animal
+        animal_sheet = Mock()
+        animal_sheet.nrows = 1
+
+        # now setting rows to a fake sheet
+        animal_sheet.row_values.return_value = []
+
+        # creating a mock sheet for sample
+        sample_sheet = Mock()
+        sample_sheet.nrows = 1
+
+        # now setting rows to a fake sheet
+        sample_sheet.row_values.return_value = []
+
+        # finally setting sheet to the fabe excel object
+        mock_book.sheet_by_name.side_effect = [
+            breed_sheet, animal_sheet, sample_sheet]
+
+        # returning the mock object when opening a workbook
+        mock_open.return_value = mock_book
+
+        # now calling methods
+        reader = ExcelTemplate()
+        reader.read_file("fake file")
+
+        # create the reference error output
+        reference = defaultdict(list)
+        reference['breed'] += TEMPLATE_COLUMNS['breed']
+        reference['animal'] += TEMPLATE_COLUMNS['animal']
+        reference['sample'] += TEMPLATE_COLUMNS['sample']
+
+        status, test = reader.check_columns()
+
+        self.assertFalse(status)
+        self.assertIsInstance(test, defaultdict)
+        self.assertDictEqual(reference, test)
 
     def check_generator(self, records, length):
         self.assertIsInstance(records, types.GeneratorType)
@@ -116,6 +182,70 @@ class ExcelTemplateTestCase(BaseExcelMixin, TestCase):
 
         self.assertTrue(check)
         self.assertEqual(len(not_found), 0)
+
+    @patch('xlrd.open_workbook')
+    def test_check_accuracies_issue(self, mock_open):
+        """Checking issues with accuracy in excels data"""
+
+        # creating a mock excel book
+        mock_book = Mock()
+
+        # customizing the mock object
+        mock_book.sheet_names.return_value = ['breed', 'animal', 'sample']
+
+        # creating a mock sheet for animal
+        animal_sheet = Mock()
+        animal_sheet.nrows = 2
+
+        # creating a fake row of data
+        fake_row = ["" for col in TEMPLATE_COLUMNS['animal']]
+
+        # get birth location accuracy index
+        accuracy_idx = TEMPLATE_COLUMNS['animal'].index(
+            "Birth location accuracy")
+
+        # set a fake accuracy item
+        fake_row[accuracy_idx] = "Fake"
+
+        # now setting rows to a fake sheet
+        animal_sheet.row_values.side_effect = [
+            TEMPLATE_COLUMNS['animal'],
+            fake_row]
+
+        # creating a mock sheet for sample
+        sample_sheet = Mock()
+        sample_sheet.nrows = 2
+
+        # creating a fake row of data
+        fake_row = ["" for col in TEMPLATE_COLUMNS['sample']]
+
+        # get birth location accuracy index
+        accuracy_idx = TEMPLATE_COLUMNS['sample'].index(
+            "Collection place accuracy")
+
+        # set a fake accuracy item
+        fake_row[accuracy_idx] = "Fake"
+
+        # now setting rows to a fake sheet
+        sample_sheet.row_values.side_effect = [
+            TEMPLATE_COLUMNS['sample'],
+            fake_row]
+
+        # finally setting sheet to the fabe excel object
+        mock_book.sheet_by_name.side_effect = [animal_sheet, sample_sheet]
+
+        # returning the mock object when opening a workbook
+        mock_open.return_value = mock_book
+
+        # now calling methods
+        reader = ExcelTemplate()
+        reader.read_file("fake file")
+
+        # define the expected value
+        reference = (False, set(["Fake"]))
+        test = reader.check_accuracies()
+
+        self.assertEqual(reference, test)
 
 
 class ExcelMixin(WebSocketMixin, BaseExcelMixin):
