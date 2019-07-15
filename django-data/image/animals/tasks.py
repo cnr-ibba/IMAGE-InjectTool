2fad1e06f39709b980cd586752152a946708732a
Alexey Sokolov <alexey@ebi.ac.uk>
2019-07-15 15:27:33 +0100

    Starting to refactor code after code review

--- django-data/image/animals/tasks.py
+++ django-data/image/animals/tasks.py
@@ -7,10 +7,8 @@ Created on Wed Feb 27 16:38:37 2019
 """
 
 from celery.utils.log import get_task_logger
-import asyncio
 
-from common.constants import ERROR, LOADED, STATUSES
-from common.helpers import send_message_to_websocket
+from common.constants import ERROR, NEED_REVISION
 from image.celery import app as celery_app, MyTask
 from image_app.models import Animal, Submission
 from submissions.helpers import send_message
@@ -39,15 +37,7 @@ class BatchUpdateAnimals(MyTask):
                                   % (str(exc)))
         submission_obj.save()
 
-        asyncio.get_event_loop().run_until_complete(
-            send_message_to_websocket(
-                {
-                    'message': STATUSES.get_value_display(ERROR),
-                    'notification_message': submission_obj.message
-                },
-                args[0]
-            )
-        )
+        send_message(submission_obj)
 
         # send a mail to the user with the stacktrace (einfo)
         submission_obj.owner.email_user(
@@ -59,11 +49,13 @@ class BatchUpdateAnimals(MyTask):
         # TODO: submit mail to admin
 
     def run(self, submission_id, animal_ids, attribute):
-        """a function to upload data into UID"""
+        """Function for batch update attribute in animals
+        Args:
+            submission_id (int): id of submission
+            animal_ids (dict): dict with id and values to update
+            attribute (str): attribute to update
+        """
 
-        logger.info(submission_id)
-        logger.info(animal_ids)
-        logger.info(attribute)
         logger.info("Start batch update for animals")
 
         for animal_id, value in animal_ids.items():
@@ -73,7 +65,7 @@ class BatchUpdateAnimals(MyTask):
 
         # Update submission
         submission_obj = Submission.objects.get(pk=submission_id)
-        submission_obj.status = LOADED
+        submission_obj.status = NEED_REVISION
         submission_obj.message = "Data updated, try to rerun validation"
         submission_obj.save()
 
--- django-data/image/samples/tasks.py
+++ django-data/image/samples/tasks.py
@@ -7,10 +7,8 @@ Created on Wed Feb 27 16:38:37 2019
 """
 
 from celery.utils.log import get_task_logger
-import asyncio
 
-from common.constants import ERROR, LOADED, STATUSES
-from common.helpers import send_message_to_websocket
+from common.constants import ERROR, NEED_REVISION
 from image.celery import app as celery_app, MyTask
 from image_app.models import Sample, Submission
 from submissions.helpers import send_message
@@ -39,15 +37,7 @@ class BatchUpdateSamples(MyTask):
                                   % (str(exc)))
         submission_obj.save()
 
-        asyncio.get_event_loop().run_until_complete(
-            send_message_to_websocket(
-                {
-                    'message': STATUSES.get_value_display(ERROR),
-                    'notification_message': submission_obj.message
-                },
-                args[0]
-            )
-        )
+        send_message(submission_obj)
 
         # send a mail to the user with the stacktrace (einfo)
         submission_obj.owner.email_user(
@@ -70,7 +60,7 @@ class BatchUpdateSamples(MyTask):
 
         # Update submission
         submission_obj = Submission.objects.get(pk=submission_id)
-        submission_obj.status = LOADED
+        submission_obj.status = NEED_REVISION
         submission_obj.message = "Data updated, try to rerun validation"
         submission_obj.save()
 
