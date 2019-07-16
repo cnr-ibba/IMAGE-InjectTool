e3935b592093aaa368b8a8ca5ed1b0e26d3ef1c6
Paolo Cozzi <bunop@libero.it>
2019-07-16 15:20:04 +0200

    :card_file_box: try to model USI submission objects

--- django-data/image/biosample/admin.py
+++ django-data/image/biosample/admin.py
@@ -7,8 +7,30 @@ Created on Fri Jul  6 11:39:15 2018
 
 from django.contrib import admin
 
-from .models import Account, ManagedTeam
+from .models import Account, ManagedTeam, Submission, SubmissionData
+
+
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = (
+        'uid_submission', 'usi_submission_id', 'created_at', 'updated_at',
+        'status'
+    )
+
+    # I cannot edit a auto_add_now field
+    readonly_fields = ('created_at', 'updated_at')
+
+
+class SubmissionDataAdmin(admin.ModelAdmin):
+    list_display = (
+        'submission', 'name', 'status'
+    )
+
+
+# --- registering applications
+
 
 # Register your models here.
 admin.site.register(Account)
 admin.site.register(ManagedTeam)
+admin.site.register(Submission, SubmissionAdmin)
+admin.site.register(SubmissionData, SubmissionDataAdmin)
--- django-data/image/biosample/models.py
+++ django-data/image/biosample/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from common.constants import STATUSES, NAME_STATUSES, WAITING, LOADED
+from image_app.models import Submission as UIDSubmission, Name
+
 
 # Create your models here.
 class Account(models.Model):
@@ -34,3 +37,62 @@ class ManagedTeam(models.Model):
     class Meta:
         verbose_name = "managed team"
         verbose_name_plural = "managed teams"
+
+
+class Submission(models.Model):
+    uid_submission = models.ForeignKey(
+        UIDSubmission,
+        on_delete=models.CASCADE,
+        related_name='usi_submissions')
+
+    usi_submission_id = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        db_index=True,
+        unique=True,
+        help_text='USI submission id')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # a column to track submission status
+    # HINT: should I limit by biosample status?
+    status = models.SmallIntegerField(
+        choices=[x.value for x in STATUSES],
+        help_text='example: Waiting',
+        default=WAITING)
+
+    def __str__(self):
+        return "%s (%s): %s" % (
+            self.usi_submission_id,
+            self.uid_submission,
+            self.get_status_display())
+
+
+class SubmissionData(models.Model):
+    submission = models.ForeignKey(
+        Submission,
+        on_delete=models.CASCADE,
+        related_name='submission_data')
+
+    name = models.ForeignKey(
+        Name,
+        on_delete=models.CASCADE)
+
+    # a column to track submission status
+    # HINT: should I limit by biosample status? should I move from UID name?
+    status = models.SmallIntegerField(
+        choices=[x.value for x in STATUSES if x.name in NAME_STATUSES],
+        help_text='example: Submitted',
+        default=LOADED)
+
+    def __str__(self):
+        return "%s: %s" % (
+            self.submission.usi_submission_id,
+            self.name.name)
+
+    class Meta:
+        verbose_name = "submission data"
+        verbose_name_plural = "submission data"
