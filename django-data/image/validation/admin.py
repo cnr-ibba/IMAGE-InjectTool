1845218a435289da7d3fded0e7a46d7d0e40045c
Paolo Cozzi <bunop@libero.it>
2019-10-14 11:11:24 +0200

    :recycle: refactor validation.admin

--- django-data/image/validation/admin.py
+++ django-data/image/validation/admin.py
@@ -11,12 +11,26 @@ from django.contrib import admin
 from .models import ValidationResult, ValidationSummary
 
 
+class ValidationSummaryAdmin(admin.ModelAdmin):
+    list_display = [
+        'submission', 'type', 'all_count', 'validation_known_count',
+        'issues_count', 'pass_count', 'warning_count', 'error_count']
+
+    list_filter = ['submission', ]
+
+    list_select_related = ('submission', 'submission__gene_bank_country')
+
+
 class ValidationResultAdmin(admin.ModelAdmin):
-    list_select_related = ('name', )
+    list_display = ['name', 'status', 'messages']
+
+    list_filter = ['name__submission', 'status']
+
+    list_select_related = ('name', 'name__submission')
 
     list_per_page = 20
 
 
 # Register your models here.
 admin.site.register(ValidationResult, ValidationResultAdmin)
-admin.site.register(ValidationSummary, admin.ModelAdmin)
+admin.site.register(ValidationSummary, ValidationSummaryAdmin)
