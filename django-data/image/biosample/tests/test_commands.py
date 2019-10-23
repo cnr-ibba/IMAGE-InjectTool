3dfc9366afe0d0a1665c06d6ea070071c351d22b
Paolo Cozzi <bunop@libero.it>
2019-10-23 12:24:34 +0200

    :fire: remove biosample management command
    Removing commands since those operation will be done by the web application

--- django-data/image/biosample/tests/test_commands.py
+++ django-data/image/biosample/tests/test_commands.py
@@ -11,10 +11,7 @@ from unittest.mock import patch
 from django.core.management import call_command
 from django.test import TestCase
 
-from common.constants import SUBMITTED
-from image_app.models import Name
-
-from ..models import ManagedTeam, Submission as USISubmission
+from ..models import ManagedTeam
 
 from .common import BaseMixin
 
@@ -42,38 +39,3 @@ class CommandsTestCase(BaseMixin, TestCase):
 
         # calling base method
         super().tearDownClass()
-
-    def test_biosample_submission(self):
-        "Test biosample_submission command command."
-
-        # mocking objects
-        args = ["--submission", 1]
-        opts = {}
-        call_command('biosample_submit', *args, **opts)
-
-        # reload submission
-        self.submission_obj.refresh_from_db()
-
-        # check submission.state changed
-        self.assertEqual(self.submission_obj.status, SUBMITTED)
-        self.assertEqual(
-            self.submission_obj.message,
-            "Waiting for biosample validation")
-
-        # test biosample.model.Submission object
-        usi_submission = USISubmission.objects.get(
-            uid_submission=self.submission_obj)
-        self.assertEqual(usi_submission.usi_submission_name, "new-submission")
-
-        # check name status changed
-        qs = Name.objects.filter(status=SUBMITTED)
-        self.assertEqual(len(qs), self.n_to_submit)
-
-        # assert called mock objects
-        self.assertTrue(self.mock_root.called)
-        self.assertTrue(self.my_root.get_team_by_name.called)
-        self.assertTrue(self.my_team.create_submission.called)
-        self.assertFalse(self.my_root.get_submission_by_name.called)
-        self.assertEqual(
-            self.new_submission.create_sample.call_count, self.n_to_submit)
-        self.assertFalse(self.new_submission.propertymock.called)
