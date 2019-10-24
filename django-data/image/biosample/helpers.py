3c64c84cfd178fbb15116cec703655e6f037a0b9
Paolo Cozzi <bunop@libero.it>
2019-10-24 11:48:12 +0200

    :recycle: move Biosamples URLs configoration to __init__ module

--- django-data/image/biosample/helpers.py
+++ django-data/image/biosample/helpers.py
@@ -12,11 +12,8 @@ import logging
 from decouple import AutoConfig
 
 import pyUSIrest.auth
-import pyUSIrest.client
 
 from django.conf import settings
-
-from common.constants import EBI_AAP_API_AUTH, BIOSAMPLE_API_ROOT
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -24,16 +21,6 @@ logger = logging.getLogger(__name__)
 # define a decouple config object
 settings_dir = os.path.join(settings.BASE_DIR, 'image')
 config = AutoConfig(search_path=settings_dir)
-
-logger.warning("Setting AAP API URL to: %s" % EBI_AAP_API_AUTH)
-
-# Ovveride Auth.auth_url
-pyUSIrest.auth.Auth.auth_url = EBI_AAP_API_AUTH
-
-logger.warning("Setting BIOSAMPLE API ROOT to: %s" % BIOSAMPLE_API_ROOT)
-
-# Override Root api_root
-pyUSIrest.client.Root.api_root = BIOSAMPLE_API_ROOT
 
 
 def get_auth(user=None, password=None, token=None):
