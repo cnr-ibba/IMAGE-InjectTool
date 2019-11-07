a9c59909f61870a978888b8b4918264c2a9ec1b8
Paolo Cozzi <bunop@libero.it>
2019-11-07 16:32:58 +0100

    :wrench: remove /image/ location from configuration files

--- django-data/image/submissions_ws/routing.py
+++ django-data/image/submissions_ws/routing.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'^image/ws/submissions/(?P<submission_key>[^/]+)/$',
+    url(r'^ws/submissions/(?P<submission_key>[^/]+)/$',
         consumers.SubmissionsConsumer),
 ]
