8a315196505cd5b983565dfdc7dbe0b2861f1d42
Paolo Cozzi <bunop@libero.it>
2020-01-24 12:26:45 +0100

    :recycle: refactor admin Submission classes

--- django-data/image/biosample/admin.py
+++ django-data/image/biosample/admin.py
@@ -6,6 +6,7 @@ Created on Fri Jul  6 11:39:15 2018
 """
 
 from django.contrib import admin
+from django.template.defaultfilters import truncatechars
 
 from .models import (
     Account, ManagedTeam, Submission, SubmissionData)
@@ -13,28 +14,68 @@ from .models import (
 
 class SubmissionAdmin(admin.ModelAdmin):
     list_display = (
-        'uid_submission', 'usi_submission_name', 'created_at', 'updated_at',
-        'message', 'status'
+        'submission_title', 'usi_submission_name', 'created_at', 'updated_at',
+        'short_message', 'status'
     )
 
-    # I cannot edit a auto_add_now field
-    readonly_fields = ('created_at', 'updated_at')
+    list_select_related = ('uid_submission',)
+
+    def short_message(self, obj):
+        return truncatechars(obj.message, 40)
+
+    # rename column in admin
+    short_message.short_description = "Message"
+
+    def submission_title(self, obj):
+        return obj.uid_submission.title
 
     list_filter = ('uid_submission__owner', 'status')
 
     list_per_page = 10
 
+    # I cannot edit a auto_add_now field
+    readonly_fields = ('uid_submission', 'created_at', 'updated_at')
+
+    fields = (
+        'uid_submission', 'usi_submission_name', 'created_at', 'updated_at',
+        'message', 'status'
+    )
+
 
 class SubmissionDataAdmin(admin.ModelAdmin):
     list_display = (
-        'submission', 'content_type', 'object_id'
+        'short_submission', 'submission_title', 'status', 'content_type',
+        'object_id'
     )
+
+    def short_submission(self, obj):
+        return obj.submission.usi_submission_name
+
+    # rename column in admin
+    short_submission.short_description = "USI Submission"
+
+    # rename column in admin
+    short_submission.short_description = "USI Submission"
+
+    def status(self, obj):
+        return obj.submission.get_status_display()
+
+    def submission_title(self, obj):
+        return obj.submission.uid_submission.title
 
     list_filter = (
         'submission__uid_submission__owner',
         'submission__uid_submission__status')
 
     list_per_page = 10
+
+    # Fields I don't want to edit
+    readonly_fields = ('submission',)
+
+    fields = (
+        'submission', 'content_type',
+        'object_id'
+    )
 
 
 class AccountAdmin(admin.ModelAdmin):
